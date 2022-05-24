639a4a1511a70d5009511c10f505141faffb8793
Ortal Wikoff <owikoff@gmail.com>
2022-05-24 10:16:11 -0400

    new files

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+//The current day is displayed at the top of the calendar 
+//Add save butten event listener (on click) and set item in a local storage
+$(document).ready(function() {
+    var todayDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
+    $("#currentDay").text(todayDate);
+    $(".saveBtn").on("click", function(){
+    var textInput = $(this).siblings(".TextArea").val();
+    var timeInput = $(this).parent().attr("id");
+    localStorage.setItem(timeInput, textInput);
+})
+
+//Time block are color-coded to indicate whether it is in the past, present, or future
+
+function pastPresenFuture(){
+//first I need to get the current number of hours
+    var timeNow = moment().hour();
+//secound I need to loop over time blocks (.each)
+ $(".time-block").each(function() {
+//The parseInt() function parses a string argument and returns an integer of the specified radix
+//The attr() get the value of an attribute for the first element in the set of matched elements or set one or more attributes for every matched element.
+     var blockOfTime = parseInt($(this).attr("id").split("-")[1])
+//third I need to check the time and add the classes for background colors.
+if (blockOfTime<timeNow){
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+} else if (blockOfTime===timeNow){
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");   
+} else{
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future") ; 
+    }
+ })
+}
+    pastPresenFuture()
+
+//get items from local storage back to TextArea
+    $("#h-8 .TextArea").val(localStorage.getItem("h-8"));
+    $("#h-9 .TextArea").val(localStorage.getItem("h-9"));
+    $("#h-10 .TextArea").val(localStorage.getItem("h-10"));
+    $("#h-11 .TextArea").val(localStorage.getItem("h-11"));
+    $("#h-12 .TextArea").val(localStorage.getItem("h-12"));
+    $("#h-13 .TextArea").val(localStorage.getItem("h-13"));
+    $("#h-14 .TextArea").val(localStorage.getItem("h-14"));
+    $("#h-15 .TextArea").val(localStorage.getItem("h-15"));
+    $("#h-16 .TextArea").val(localStorage.getItem("h-16"));
+    $("#h-17 .TextArea").val(localStorage.getItem("h-17"));
+
+})
+
+
